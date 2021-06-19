25201f911fd61ddf76a6328c86ff24f8e91f134f
AlecCross <cross2472@gmail.com>
2021-06-19 13:36:53 +0300

    Смещение правой стороны
    Смещение на высоту в пикселях окна По нажатию на кнопки вверх и вниз

--- app.js
+++ app.js
@@ -15,4 +15,50 @@ const downBtn = document.querySelector('.down-button')
 //Получим это кол-во и зафиксируем в скрипте
 const sidebar = document.querySelector('.sidebar')
 //Можно оперировать стилями через:
-sidebar.style.top = `-300vh`
+//sidebar.style.top = `-300vh`//Сдвинет текущий фон слева на 3
+//Считатаем кол-во слайдов. 
+//Получим блок .main-slide
+const mainSlide = document.querySelector('.main-slide')
+//И посчитаем внутри него кол-во div
+const slidesCount = mainSlide.querySelectorAll('div').length-1
+//slidesCount*100 будет белый фон, так как там есть какой-то
+//зарезервированый по умолчанию слайд
+//sidebar.style.top = `-${slidesCount*90}vh`//так будет видно переход между белым слайдом и жедтым
+
+//.length-1 чтобы не писать постоянно -1
+sidebar.style.top = `-${(slidesCount)*100}vh`
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+let activeSlideIndex = 0
+
+//Объединение логики нажатия в функ с параметром вызова
+function changeSlide(direction){
+    if(direction === 'up'){
+        activeSlideIndex++
+        if(activeSlideIndex === slidesCount){
+            activeSlideIndex = 0
+        }
+    }
+    else if(direction === 'down'){
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesCount
+        }
+    }
+    //Анимируем. Нужно сместить кратинку на высоту 
+    //видимой части окна в пикселях из блока container
+    //mainSlide.style.transform = 'translateY(-1000px)'
+
+    
+    const container = document.querySelector('.container')
+    //Спойлер на тему создания переменных через const
+    const height = container.clientHeight
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+}
+
